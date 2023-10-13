bf16e0d1830343cfb5472fc5f98a9ddf03461ebd
Jaime Pineda <jpin730@pm.me>
2023-10-12 22:19:24 -0600

    refactor: :fire: remove unused code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+  template: '<h1>Angular Pokemon Trainer</h1>',
 })
-export class AppComponent {
-  title = 'angular-pokemon-trainer';
-}
+export class AppComponent {}
